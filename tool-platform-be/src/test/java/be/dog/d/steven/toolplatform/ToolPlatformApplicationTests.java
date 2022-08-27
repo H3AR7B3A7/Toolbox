d7838edf772d142413728dc8eb97a87574e03fad
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2022-08-27 16:31:11 +0200

    :rocket: Initial commit

---
+++ tool-platform-be/src/test/java/be/dog/d/steven/toolplatform/ToolPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package be.dog.d.steven.toolplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
