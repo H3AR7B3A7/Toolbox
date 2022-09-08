d146b8a3c2efee40a324ba332810b0243fcd43e8
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2022-09-08 22:04:58 +0200

    :bug: 1 - Fixed api endpoints not being protected & code cleanup

--- tool-platform-be/src/main/java/be/dog/d/steven/toolplatform/security/configuration/SecurityConfiguration.java
+++ tool-platform-be/src/main/java/be/dog/d/steven/toolplatform/security/configuration/SecurityConfiguration.java
@@ -7,10 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -19,31 +18,26 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 public class SecurityConfiguration {
 
     private final AuthenticationSuccessHandler authenticationSuccessHandler;
-    private final ClientRegistrationRepository clientRegistrationRepository;
+    private final LogoutSuccessHandler logoutSuccessHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .mvcMatchers("/login").permitAll()
-                .and()
-                .oauth2Login()
-                .successHandler(this.authenticationSuccessHandler)
-                .and()
-                .logout()
-                .logoutSuccessHandler(oidcLogoutSuccessHandler());
+                .authorizeRequests(authorize -> authorize
+                        .mvcMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .successHandler(this.authenticationSuccessHandler)
+                )
+                .logout(logout -> logout
+                        .logoutSuccessHandler(this.logoutSuccessHandler)
+                );
         return http.build();
     }
 
     @Bean // TODO: Replace H2 and remove this
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().antMatchers("/h2-console/**");
-    }
-
-    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
-        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
-        successHandler.setPostLogoutRedirectUri("http://localhost:8080/");
-        log.info("User logged out:");
-        return successHandler;
     }
 }
--- tool-platform-be/src/main/java/be/dog/d/steven/toolplatform/security/model/UserRegistrationRequest.java
+++ tool-platform-be/src/main/java/be/dog/d/steven/toolplatform/security/model/UserRegistrationRequest.java
@@ -1,10 +1,12 @@
 package be.dog.d.steven.toolplatform.security.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UserRegistrationRequest {
-
+    
     private String firstname;
     private String lastname;
     private String username;
