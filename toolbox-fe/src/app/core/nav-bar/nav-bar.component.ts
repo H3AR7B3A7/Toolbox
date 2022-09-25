36776defc538b9cc6f83b73903a2478b8c8f6910
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2022-09-25 05:31:14 +0200

    :microscope: Added an MDX version of navbar storybook for testing

--- toolbox-fe/src/app/core/nav-bar/nav-bar.component.ts
+++ toolbox-fe/src/app/core/nav-bar/nav-bar.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@app/core/auth.service';
 import { map, Observable, startWith } from 'rxjs';
 
+/**
+ * The navigation bar at the top of the page
+ */
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
@@ -13,6 +16,9 @@ export class NavBarComponent implements OnInit {
 
   constructor(private authService: AuthService) {}
 
+  /**
+   * Checks for authentication and fetches the user profile picture
+   */
   ngOnInit(): void {
     this.loggedIn = this.authService.isLoggedIn;
     this.picture = this.authService.auth.pipe(
@@ -21,6 +27,9 @@ export class NavBarComponent implements OnInit {
     );
   }
 
+  /**
+   * Called by the logout button
+   */
   logout() {
     this.authService.logout();
   }
